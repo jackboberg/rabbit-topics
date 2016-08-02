fcdea7304d67a77c8c05b2f3965657c5c294b861
Jack Boberg <jack.boberg@gmail.com>
2016-08-02 10:25:43 -0400

    Create placeholder main function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+module.exports = function (url, opts) {
+  return {
+    publish: function (topic, msg, done) {
+      done();
+    },
+    subscribe: function (topic, worker) {
+      // listen and queue to worker
+    }
+  };
+};
--- test/index.js
+++ test/index.js
@@ -6,9 +6,11 @@ const describe = lab.describe;
 const it = lab.it;
 const expect = Code.expect;
 
+const Topics = require('..');
+
 describe('rabbit-topics', function () {
   it('needs tests', { skip: true }, function (done) {
-    expect(true).to.be.true();
+    expect(Topics).to.be.a.funcion();
     done();
   });
 });
