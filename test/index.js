34d89d0e16e3a3983b489c99c4795bd5098393bd
Jack Boberg <hello@jackboberg.info>
2017-08-07 16:12:12 -0400

    test: Replace test suite

--- test/index.js
+++ test/index.js
@@ -1,16 +1,26 @@
-const Code = require('code')
-const Lab = require('lab')
-const lab = exports.lab = Lab.script()
+const { replace, when } = require('testdouble')
+const { test } = require('tap')
 
-const describe = lab.describe
-const it = lab.it
-const expect = Code.expect
+const Publish = replace('../lib/publish')
+const Subscribe = replace('../lib/subscribe')
 
 const Topics = require('..')
 
-describe('rabbit-topics', function () {
-  it('needs tests', { skip: true }, function (done) {
-    expect(Topics).to.be.a.funcion()
-    done()
-  })
+const url = 'amqp://guest:guest@localhost'
+const opts = {}
+
+test('returns a publisher', ({ end, equals }) => {
+  when(Publish(url, opts)).thenReturn('publisher')
+  const { publish } = Topics(url, opts)
+
+  equals(publish, 'publisher')
+  end()
+})
+
+test('returns a subscriber', ({ end, equals }) => {
+  when(Subscribe(url, opts)).thenReturn('subscriber')
+  const { subscribe } = Topics(url, opts)
+
+  equals(subscribe, 'subscriber')
+  end()
 })
--- test/subscribe.js
+++ test/subscribe.js
@@ -1,16 +1,9 @@
-const Code = require('code')
-const Lab = require('lab')
-const lab = exports.lab = Lab.script()
-
-const describe = lab.describe
-const it = lab.it
-const expect = Code.expect
+const { test } = require('tap')
+const isFunction = require('lodash.isfunction')
 
 const Subscribe = require('../lib/subscribe')
 
-describe('Subscribe', function () {
-  it('needs tests', { skip: true }, function (done) {
-    expect(Subscribe).to.be.a.function()
-    done()
-  })
+test('needs tests', { skip: true }, ({ assert, end }) => {
+  assert(isFunction(Subscribe))
+  end()
 })
